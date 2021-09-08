f2ecc5cc303dad0e3bd4d9d86f9601f76141f39d
Artur Manukyan <aptumen@yandex.ru>
2021-09-08 11:48:40 +0300

    step 1,2,3

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div className="main">
+      <div className="main-check">
+        <div className="main-check__drop"></div>
+        <div className="main-check__text"></div>
+        <button className="main-check__button"></button>
+      </div>
+      <div className="main-camera"></div>
+    </div>
+  );
+}
+
+export default Main;
